a8b1e5924213c714d1496f16c350c3b7e0514d0a
jeysonTK <alexzamfir94@gmail.com>
2026-06-12 16:30:01 +0300

    update dual_arm_description with newest urdf

--- src/simulation/dual_arm_gazebo/launch/headless_sim.launch.py
+++ src/simulation/dual_arm_gazebo/launch/headless_sim.launch.py
@@ -72,7 +72,7 @@ def generate_launch_description():
             '-topic', 'robot_description',
             '-x', '0.0',
             '-y', '0.0',
-            '-z', '1.215',
+            '-z', '0.855',
             '-Y', '0.0'
         ],
         output='screen'
